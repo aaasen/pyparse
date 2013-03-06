1bbc8d6f34868daff5d2c5b26695010fce1fe46f
Lane Aasen <laneaasen@gmail.com>
2013-03-06 08:52:42 -0800

    adds headers that enable fetching from tpb programmatically

--- init.py
+++ init.py
@@ -2,7 +2,17 @@
 
 import requests
 
-print "hey!"
+chromium_headers = { 'Host' : 'thepiratebay.se',
+	'Connection' : 'keep-alive',
+	'Cache-Control' : 'max-age=0',
+	'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22',
+	'Accept-Encoding' : 'gzip,deflate,sdch',
+	'Accept-Language' : 'en-US,en;q=0.8',
+	'Accept-Charset' : 'UTF-8,*;q=0.5' }
 
-r = requests.get('http://thepiratebay.se/')
+r = requests.get('http://thepiratebay.se/',
+	params = {'action' : 'log', 'js' : 'no'},
+	headers = chromium_headers)
+
 print r.status_code
