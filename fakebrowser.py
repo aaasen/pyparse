73725839cebe23625ebdd36470fd57681e6d1c1c
Lane Aasen <laneaasen@gmail.com>
2013-03-06 09:08:18 -0800

    moves spoof headers to FakeBrowser and changes user agent to a more common one

---
+++ fakebrowser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+class FakeBrowser:
+	headers = { 'Connection' : 'keep-alive',
+		'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'User-Agent' : 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+		'Accept-Encoding' : 'gzip,deflate,sdch',
+		'Accept-Language' : 'en-US,en;q=0.8',
+		'Accept-Charset' : 'UTF-8,*;q=0.5' }
--- init.py
+++ init.py
@@ -2,17 +2,11 @@
 
 import requests
 
-chromium_headers = { 'Host' : 'thepiratebay.se',
-	'Connection' : 'keep-alive',
-	'Cache-Control' : 'max-age=0',
-	'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22',
-	'Accept-Encoding' : 'gzip,deflate,sdch',
-	'Accept-Language' : 'en-US,en;q=0.8',
-	'Accept-Charset' : 'UTF-8,*;q=0.5' }
+from fakebrowser import FakeBrowser
+
+print "requesting"
 
 r = requests.get('http://thepiratebay.se/',
-	params = {'action' : 'log', 'js' : 'no'},
-	headers = chromium_headers)
+	headers = FakeBrowser.headers)
 
 print r.status_code
