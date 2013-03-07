79723ea6db70cd0ffba109204e3c97a874dfb0b8
Lane Aasen <laneaasen@gmail.com>
2013-03-07 11:24:28 -0800

    minor refactoring

--- fakebrowser.py
+++ fakebrowser.py
@@ -4,4 +4,5 @@ class FakeBrowser:
 	headers = { 
 		'User-Agent' : 'curl/7.19.4 (universal-apple-darwin10.0) libcurl/7.19.4 OpenSSL/0.9.8k zlib/1.2.3',
 		'Host': '127.0.0.1',
-		'Accept': '*/*' }
+		'Accept': '*/*' 
+	}
