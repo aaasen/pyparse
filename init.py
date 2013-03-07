9d49e975696b5691dd57a015c113657446e0ffaf
Lane Aasen <laneaasen@gmail.com>
2013-03-07 09:01:59 -0800

    adds lxml parsing to get torrent urls for a search term

--- init.py
+++ init.py
@@ -2,16 +2,24 @@
 
 import requests
 from lxml import etree
+from cssselect import GenericTranslator, SelectorError
 
 from fakebrowser import FakeBrowser
 
+s = requests.Session()
+s.headers.update(FakeBrowser.headers)
+
 print "requesting"
 
-r = requests.get('http://thepiratebay.se/',
-	headers = FakeBrowser.headers)
+r = requests.get('http://thepiratebay.se/search/test/0/99/0')
 
 print r.status_code
 
-root = etree.HTML(r.text)
+if r.status_code == requests.codes.ok:
+	root = etree.HTML(r.text)
 
-print etree.tostring(root)
+	expression = GenericTranslator().css_to_xpath('a.detLink')
+
+	links = root.xpath(expression)
+
+	print map(lambda x: [v[1] for i, v in enumerate(x.items()) if v[0] == 'href'], links)
