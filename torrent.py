db35bbcec74310511736602de310670607056bb0
Lane Aasen <laneaasen@gmail.com>
2013-03-07 10:45:01 -0800

    adds torrent class with placeholder values

---
+++ torrent.py
@@ -0,0 +1,20 @@
+
+class Torrent:
+	url = None
+
+	name = None
+	magnet = None
+	info_hash = None
+	
+	upload_date = None
+	upload_user = None
+
+	seeders = None
+	leechers = None
+
+	comments = None
+	tags = None
+	category = None
+
+	size = None
+	num_files = None
