250d6e5b8da246148633d779edfb8f321623825e
Lane Aasen <laneaasen@gmail.com>
2013-03-26 14:08:51 -0700

    generalizes source

--- src/pyparse/source.py
+++ src/pyparse/source.py
@@ -4,19 +4,13 @@ import yaml
 from errors import LoadError
 
 class Source:
-	_config = None
-
 	def __init__(self, config_file):
 		with open(config_file, 'r') as f:
 			suffix = config_file.split('.')[-1]
 
 			if suffix == 'json':
-				self._config = json.load(f)
+				self.data = json.load(f)
 			elif suffix in ['yml', 'yaml']:
-				self._config = yaml.load(f)
+				self.data = yaml.load(f)
 			else:
 				raise LoadError(config_file, '\'%s\' is not a supported file type' % suffix)
-
-			self.info = self._config['info']
-			self.search = self._config['search']
-			self.torrent = self._config['torrent']
